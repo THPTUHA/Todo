d31375cb6fd7a885c72e6d8d9557e25e8b3d8a0d
NghiaX <minhnghiabadao1002@gmail.com>
2021-09-27 22:33:24 +0700

    add apps

---
+++ apps/controller/index.js
@@ -0,0 +1,10 @@
+var express=require("express");
+var router=express.Router();
+router.use("/admin",require(__dirname+"/admin"));
+router.use("/user",require(__dirname+"/user"));
+router.get("/",function(req,res){
+//    res.json({"message":"HomePage"});
+    res.render("test");
+});
+
+module.exports=router;
---
+++ apps/views/test.ejs
@@ -0,0 +1,1 @@
+<h1>This is HomePage</h1>
