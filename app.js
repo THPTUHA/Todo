d95c2a9cab8df0fb3de060108f0cf342ca6543dc
NghiaX <minhnghiabadao1002@gmail.com>
2021-09-29 22:47:11 +0700

    add v2

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var express=require('express');
 var config=require('config');
 var bodyParser=require('body-parser');
+var path=require('path');
 
 var app=express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set("views",__dirname+"/apps/views");
 app.set("view engine","ejs");
 var controllers=require(__dirname+"/apps/controller");
 
-
 app.use(controllers);
+app.use('/static',express.static(path.join(__dirname, 'public')));
+
 var host=config.get("server.host");
 var port=config.get("server.port");
 app.listen(port,host,function(){
