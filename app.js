0dda7444501966ff18161345e43ff47178e04593
NghiaX <minhnghiabadao1002@gmail.com>
2021-09-30 23:48:49 +0700

    add app v3

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ var express=require('express');
 var config=require('config');
 var bodyParser=require('body-parser');
 var path=require('path');
+var session =require('express-session');
 
 var app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+//session
+app.set("trust proxy",1);
+app.use(session({
+  secret:config.get("secret_key"),
+  resave:false,
+  saveUninitialized:true,
+  cookie:{secure:true}
+}));
 
 app.set("views",__dirname+"/apps/views");
 app.set("view engine","ejs");
